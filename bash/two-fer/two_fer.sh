3dd6f1967f500adef42adff4a6eec09050737937
Christina Sørensen <ces@fem.gg>
2025-01-20 17:26:28 +0100

    feat(bash): two_fer imrpovements
    Signed-off-by: Christina Sørensen <ces@fem.gg>

--- bash/two-fer/two_fer.sh
+++ bash/two-fer/two_fer.sh
@@ -1,10 +1,2 @@
 #!/usr/bin/env bash
-main () {
-    name="$1";
-    if [[ "$1" == "" ]]; then
-        name="you";
-    fi
-    echo "One for $name, one for me.";
-}
-
-main "$@"
+echo "One for ${1:-you}, one for me.";
