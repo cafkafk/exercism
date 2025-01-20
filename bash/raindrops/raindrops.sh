9844bba7e7c4a8cca29a8d4dfe8b7c0f17923f80
Christina Sørensen <ces@fem.gg>
2025-01-20 17:21:23 +0100

    feat(bash): raindrops improvements
    Signed-off-by: Christina Sørensen <ces@fem.gg>

--- bash/raindrops/raindrops.sh
+++ bash/raindrops/raindrops.sh
@@ -1,19 +1,23 @@
 #!/usr/bin/env bash
 
 main () {
-    if [[ $(($1 % 3)) -eq 0 ]]; then
+    no_matches=true;
+    if (( $1 % 3 == 0 )) ; then
+        no_matches=false;
         echo -n "Pling";
     fi
-    if [[ $(($1 % 5)) -eq 0 ]]; then
+    if (( $1 % 5 == 0 )); then
+        no_matches=false;
         echo -n "Plang"
     fi
-    if [[ $(($1 % 7)) -eq 0 ]]; then
+    if (($1 % 7 == 0)); then
+        no_matches=false;
         echo -n "Plong"
     fi
-    if [[ $(($1 % 3)) -eq 0 || $(($1 % 5)) -eq 0 || $(($1 % 7)) -eq 0 ]]; then
-        echo ""
-    else
+    if $no_matches; then
         echo "$1"
+    else
+        echo ""
     fi
 }
 
