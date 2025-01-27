2cdabe59e2db33bfd844c141fa70c261246901ef
Christina Sørensen <ces@fem.gg>
2025-01-27 13:26:22 +0100

    fix(bash): mentor feedback raindrops
    Signed-off-by: Christina Sørensen <ces@fem.gg>

--- bash/raindrops/raindrops.sh
+++ bash/raindrops/raindrops.sh
@@ -1,24 +1,13 @@
 #!/usr/bin/env bash
 
-main () {
-    no_matches=true;
-    if (( $1 % 3 == 0 )) ; then
-        no_matches=false;
-        echo -n "Pling";
-    fi
-    if (( $1 % 5 == 0 )); then
-        no_matches=false;
-        echo -n "Plang"
-    fi
-    if (($1 % 7 == 0)); then
-        no_matches=false;
-        echo -n "Plong"
-    fi
-    if $no_matches; then
-        echo "$1"
-    else
-        echo ""
-    fi
-}
+if (( $1 % 3 == 0 )) ; then
+    res+="Pling";
+fi
+if (( $1 % 5 == 0 )); then
+    res+="Plang"
+fi
+if (($1 % 7 == 0)); then
+    res+="Plong"
+fi
 
-main "$@";
+echo ${res:-$1}
