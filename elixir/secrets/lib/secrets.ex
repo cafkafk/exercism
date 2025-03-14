c61e2a16e2db09b87fea7d301900ffcf12225e8a
Christina Sørensen <ces@fem.gg>
2025-03-14 12:48:11 +0100

    feat(elixir): secrets concise
    Signed-off-by: Christina Sørensen <ces@fem.gg>

--- elixir/secrets/lib/secrets.ex
+++ elixir/secrets/lib/secrets.ex
@@ -1,15 +1,9 @@
 defmodule Secrets do
-  def secret_add(secret), do: fn n -> n + secret end
-
-  def secret_subtract(secret), do: fn n -> n - secret end
-
-  def secret_multiply(secret), do: fn n -> n * secret end
-
-  def secret_divide(secret), do: fn n -> div(n, secret) end
-
-  def secret_and(secret), do: fn n -> Bitwise.band(n, secret) end
-
-  def secret_xor(secret), do: fn n -> Bitwise.bxor(n, secret) end
-
-  def secret_combine(fn1, fn2), do: fn n -> fn2.(fn1.(n)) end
+  def secret_add(s), do: &(&1 + s)
+  def secret_subtract(s), do: &(&1 - s)
+  def secret_multiply(s), do: &(&1 * s)
+  def secret_divide(s), do: &div(&1, s)
+  def secret_and(s), do: &Bitwise.band(&1, s)
+  def secret_xor(s), do: &Bitwise.bxor(&1, s)
+  def secret_combine(fn1, fn2), do: &fn2.(fn1.(&1))
 end
