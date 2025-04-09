57187fd25b4401747a1ab072199bba4618e27071
Christina Sørensen <ces@fem.gg>
2025-04-09 17:49:42 +0200

    fix(elixir): dancing-dots
    Signed-off-by: Christina Sørensen <ces@fem.gg>

--- elixir/dancing-dots/lib/dancing_dots/animation.ex
+++ elixir/dancing-dots/lib/dancing_dots/animation.ex
@@ -4,13 +4,49 @@ defmodule DancingDots.Animation do
   @type error :: any
   @type frame_number :: pos_integer
 
-  # Please implement the module
+  @callback init(opts) :: {:ok, opts} | {:error, error}
+  @callback handle_frame(dot, frame_number, opts) :: DancingDots.Dot.t()
+
+  defmacro __using__(_) do
+    quote do
+      @behaviour DancingDots.Animation
+      def init(opts), do: {:ok, opts}
+      defoverridable init: 1
+    end
+  end
 end
 
 defmodule DancingDots.Flicker do
-  # Please implement the module
+  import Integer, only: [mod: 2]
+  use DancingDots.Animation
+
+  @impl DancingDots.Animation
+  def handle_frame(dot, frame_number, opts) do
+    if mod(frame_number, 4) == 0 do
+      %DancingDots.Dot{dot | opacity: dot.opacity * 0.5}
+    else
+      dot
+    end
+  end
 end
 
 defmodule DancingDots.Zoom do
-  # Please implement the module
+  use DancingDots.Animation
+
+  @impl DancingDots.Animation
+  def init([velocity: velocity] = opts) when is_number(velocity) do
+    {:ok, opts}
+  end
+
+  def init(opts) do
+    velocity = Keyword.get(opts, :velocity)
+
+    {:error,
+     "The :velocity option is required, and its value must be a number. Got: #{inspect(velocity)}"}
+  end
+
+  @impl DancingDots.Animation
+  def handle_frame(%{radius: radius} = dot, frame_number, [velocity: velocity] = opts) do
+    %DancingDots.Dot{dot | radius: radius + (frame_number - 1) * velocity}
+  end
 end
--- elixir/dancing-dots/lib/dancing_dots/dot.ex
+++ elixir/dancing-dots/lib/dancing_dots/dot.ex
@@ -1,19 +1,4 @@
 defmodule DancingDots.Dot do
   defstruct [:x, :y, :radius, :opacity]
   @type t :: %__MODULE__{}
-
-  @type error :: String.t()
-  @type opts :: collection :: any
-  @type frame_number :: integer
-
-  @callback init(opts) :: {:ok, opts} | {:error, error}
-  @callback handle_frame(Dot, frame_number, opts) :: Dot
-
-  defmacro __using__(_) do
-    quote do
-      @behavior Animation
-      def init(opts), do: {:ok, opts}
-      defoverridable count: 1
-    end
-  end
 end
