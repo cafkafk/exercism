e23fffc9a9c1a7f0d9b0c226868a0627aa10070b
Christina Sørensen <ces@fem.gg>
2025-04-25 14:40:04 +0200

    fix(elixir): basketball
    Signed-off-by: Christina Sørensen <ces@fem.gg>

--- elixir/basketball-website/lib/basketball_website.ex
+++ elixir/basketball-website/lib/basketball_website.ex
@@ -1,16 +1,9 @@
 defmodule BasketballWebsite do
   defp traverse_data([], data), do: data
   defp traverse_data([], nil), do: nil
+  defp traverse_data([head | tail], data), do: traverse_data(tail, data[head])
 
-  defp traverse_data([head | tail], data) do
-    traverse_data(tail, data[head])
-  end
+  def extract_from_path(data, path), do: traverse_data(String.split(path, "."), data)
 
-  def extract_from_path(data, path) do
-    traverse_data(String.split(path, "."), data)
-  end
-
-  def get_in_path(data, path) do
-    # Please implement the get_in_path/2 function
-  end
+  def get_in_path(data, path), do: get_in(data, String.split(path, "."))
 end
