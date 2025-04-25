805dd7964c0dc18953b1f2cd13c572cd514e3a3b
Christina Sørensen <ces@fem.gg>
2025-04-25 14:41:44 +0200

    fix(elixir): take-a-number-deluxe
    Signed-off-by: Christina Sørensen <ces@fem.gg>

--- elixir/take-a-number-deluxe/lib/take_a_number_deluxe.ex
+++ elixir/take-a-number-deluxe/lib/take_a_number_deluxe.ex
@@ -1,24 +1,24 @@
 defmodule TakeANumberDeluxe do
-  # Client API
+  use GenServer
 
   @spec start_link(keyword()) :: {:ok, pid()} | {:error, atom()}
   def start_link(init_arg) do
-    TakeANumberDeluxe.State.new(init_arg[:min_number], init_arg[:max_number])
+    GenServer.start_link(__MODULE__, init_arg)
   end
 
   @spec report_state(pid()) :: TakeANumberDeluxe.State.t()
   def report_state(machine) do
-    # Please implement the report_state/1 function
+    GenServer.call(machine, :report_state)
   end
 
   @spec queue_new_number(pid()) :: {:ok, integer()} | {:error, atom()}
   def queue_new_number(machine) do
-    # Please implement the queue_new_number/1 function
+    GenServer.call(machine, :queue_new_number)
   end
 
   @spec serve_next_queued_number(pid(), integer() | nil) :: {:ok, integer()} | {:error, atom()}
   def serve_next_queued_number(machine, priority_number \\ nil) do
-    # Please implement the serve_next_queued_number/2 function
+    GenServer.call(machine, {:serve_next_queued_number, priority_number})
   end
 
   @spec reset_state(pid()) :: :ok
