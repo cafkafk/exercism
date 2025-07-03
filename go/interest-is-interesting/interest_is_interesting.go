a529ee78e9bdc617294c9280621f0fd32d27b1d0
Christina Sørensen <ces@fem.gg>
2025-07-03 09:42:15 +0200

    feat(go): interest-is-interesting
    Signed-off-by: Christina Sørensen <ces@fem.gg>

--- go/interest-is-interesting/interest_is_interesting.go
+++ go/interest-is-interesting/interest_is_interesting.go
@@ -24,12 +24,12 @@ func InterestRate(balance float64) float32 {
 
 // Interest calculates the interest for the provided balance.
 func Interest(balance float64) float64 {
-	return balance * Interest(balance)
+	return balance * float64(InterestRate(balance)) / 100
 }
 
 // AnnualBalanceUpdate calculates the annual balance update, taking into account the interest rate.
 func AnnualBalanceUpdate(balance float64) float64 {
-	return balance + balance*Interest(balance)
+	return balance + Interest(balance)
 }
 
 // YearsBeforeDesiredBalance calculates the minimum number of years required to reach the desired balance.
