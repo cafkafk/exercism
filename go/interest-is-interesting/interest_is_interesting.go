8acfc82626bfddc1ec13d401009520c747cfbece
Christina Sørensen <ces@fem.gg>
2025-07-03 10:17:04 +0200

    feat(go): interest-is-interesting
    Signed-off-by: Christina Sørensen <ces@fem.gg>

--- go/interest-is-interesting/interest_is_interesting.go
+++ go/interest-is-interesting/interest_is_interesting.go
@@ -1,27 +1,36 @@
 package interest
 
-// - 3.213% for a balance less than `0` dollars (balance gets more negative).
-// - 0.5% for a balance greater than or equal to `0` dollars, and less than `1000` dollars.
-// - 1.621% for a balance greater than or equal to `1000` dollars, and less than `5000` dollars.
-// - 2.475% for a balance greater than or equal to `5000` dollars.
+const (
+	negativeBalanceInterestRate float32 = 3.213
+	smallBalanceInterestRate    float32 = 0.5
+	mediumBalanceInterestRate   float32 = 1.621
+	largeBalanceInterestRate    float32 = 2.475
+)
+
+const (
+	smallBalanceThreshold  = 1000
+	mediumBalanceThreshold = 5000
+)
+
+const percent = 100
 
 // InterestRate returns the interest rate for the provided balance.
 func InterestRate(balance float64) float32 {
 	switch {
 	case balance < 0:
-		return 3.213
-	case balance < 1000:
-		return 0.5
-	case balance < 5000:
-		return 1.621
+		return negativeBalanceInterestRate
+	case balance < smallBalanceThreshold:
+		return smallBalanceInterestRate
+	case balance < mediumBalanceThreshold:
+		return mediumBalanceInterestRate
 	default:
-		return 2.475
+		return largeBalanceInterestRate
 	}
 }
 
 // Interest calculates the interest for the provided balance.
 func Interest(balance float64) float64 {
-	return balance * float64(InterestRate(balance)) / 100
+	return balance * float64(InterestRate(balance)) / percent
 }
 
 // AnnualBalanceUpdate calculates the annual balance update, taking into account the interest rate.
