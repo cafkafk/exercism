d7017669f1dc97225f670e8c91f27c030fa5e9b8
Christina Sørensen <ces@fem.gg>
2025-07-11 12:24:39 +0200

    feat(rust): difference-of-squares
    Signed-off-by: Christina Sørensen <ces@fem.gg>

---
+++ rust/difference-of-squares/src/lib.rs
@@ -0,0 +1,28 @@
+// Find the difference between the square of the sum and the sum of the squares of the first N natural numbers.
+//
+// The square of the sum of the first ten natural numbers is
+// (1 + 2 + ... + 10)² = 55² = 3025.
+//
+// The sum of the squares of the first ten natural numbers is
+// 1² + 2² + ... + 10² = 385.
+//
+// Hence the difference between the square of the sum of the first ten natural
+// numbers and the sum of the squares of the first ten natural numbers is 3025 -
+// 385 = 2640.
+//
+// You are not expected to discover an efficient solution to this yourself from
+// first principles; research is allowed, indeed, encouraged.  Finding the best
+// algorithm for the problem is a key skill in software engineering.
+
+pub fn square_of_sum(n: u32) -> u32 {
+    let sum = n * (n + 1) / 2;
+    sum * sum
+}
+
+pub fn sum_of_squares(n: u32) -> u32 {
+    todo!("sum of squares of 1...{n}")
+}
+
+pub fn difference(n: u32) -> u32 {
+    todo!("difference between square of sum of 1...{n} and sum of squares of 1...{n}")
+}
