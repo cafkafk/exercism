0dfa28494348db4960ddec27700820c315f0eb40
Christina Sørensen <ces@fem.gg>
2025-10-04 09:50:38 +0200

    feat(elixir): gotta-snatch-em-all
    Signed-off-by: Christina Sørensen <ces@fem.gg>

---
+++ elixir/gotta-snatch-em-all/lib/gotta_snatch_em_all.ex
@@ -0,0 +1,48 @@
+defmodule GottaSnatchEmAll do
+  @type card :: String.t()
+  @type collection :: MapSet.t(card())
+
+  @spec new_collection(card()) :: collection()
+  def new_collection(card) do
+    MapSet.new([card])
+  end
+
+  @spec add_card(card(), collection()) :: {boolean(), collection()}
+  def add_card(card, collection) do
+    {MapSet.member?(collection, card), MapSet.put(collection, card)}
+  end
+
+  @spec trade_card(card(), card(), collection()) :: {boolean(), collection()}
+  def trade_card(your_card, their_card, collection) do
+    already_has = MapSet.member?(collection, their_card)
+    no_card_to_trade = !MapSet.member?(collection, your_card)
+
+    {already_has && no_card_to_trade,
+     MapSet.put(MapSet.delete(collection, your_card), their_card)}
+  end
+
+  @spec remove_duplicates([card()]) :: [card()]
+  def remove_duplicates(cards) do
+    # Please implement remove_duplicates/1
+  end
+
+  @spec extra_cards(collection(), collection()) :: non_neg_integer()
+  def extra_cards(your_collection, their_collection) do
+    # Please implement extra_cards/2
+  end
+
+  @spec boring_cards([collection()]) :: [card()]
+  def boring_cards(collections) do
+    # Please implement boring_cards/1
+  end
+
+  @spec total_cards([collection()]) :: non_neg_integer()
+  def total_cards(collections) do
+    # Please implement total_cards/1
+  end
+
+  @spec split_shiny_cards(collection()) :: {[card()], [card()]}
+  def split_shiny_cards(collection) do
+    # Please implement split_shiny_cards/1
+  end
+end
